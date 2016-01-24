ea8368f442ab5b767b7100118c1713fda953f412
Yang Liu <yliu158@binghamton.edu>
2016-01-24 18:21:58 -0500

    commit 3

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,31 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  inverse_m <- NULL
+  set <- function(y){
+    x <<- y
+    inverse_m <<- NULL
+  }
+  get <- function() x
+  setinve_m <- function(inverse) inverse_m <<- inverse
+  getinve_m <- function() inverse_m
+  list(set = set, get = get, setinve = setinve,
+       getinve = getinve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinve()
+  if(!is.null(inverse)){
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse_m <- solve(data, ...)
+  x$setinve(inverse_m)
+  inverse_m
 }
+
